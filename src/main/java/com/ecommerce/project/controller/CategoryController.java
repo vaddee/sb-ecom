929e49abe36393da79e326f1013b6588e8d8308d
vaddee <valtteri.laakso@hotmail.fi>
2025-01-18 18:27:11 +0200

    Update CategoryController.java
    Added ResponseEntity validation to all api calls

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -19,16 +19,18 @@ public class CategoryController {
 
 
     @GetMapping("/api/public/categories")
-    public List<Category> getAllCategories() {
-        return categoryService.getAllCategories();
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
 
     @PostMapping("/api/public/categories")
-    public String createCategory(@RequestBody Category category) {
+    public ResponseEntity<String> createCategory(@RequestBody Category category) {
 
         categoryService.createCategory(category);
-        return "Category added successfully";
+        return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
+
     }
     @DeleteMapping("/api/admin/categories/{categoryId}")
     public ResponseEntity <String> deleteCategory(@PathVariable Long categoryId){
